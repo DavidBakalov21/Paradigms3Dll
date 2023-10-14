9a8eb81777dd0eb0440a88a1a261d1a2fcde3ce2
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-15 01:30:45 +0300

    done

--- Dll2/Dll2.cpp
+++ Dll2/Dll2.cpp
@@ -6,21 +6,14 @@
 
 
 DLL2_API std::string Encrypt(std::string str, int key) {
-
     std::string encrypted = str;
-
 
     for (int i = 0; i < encrypted.size(); i++)
     {
-        if (std::isalpha(encrypted[i])) {
-            char base = 'a';
-            if (std::isupper(encrypted[i]) == true) {
-                base = 'A';
-            }
-            encrypted[i] = static_cast<char>(base + (encrypted[i] - base + (key % 26)) % 26);
-        }
+        char base = 32; 
+        char end = 126;  
+        encrypted[i] = static_cast<char>(base + (encrypted[i] - base + (key % (end - base + 1))) % (end - base + 1));
     }
-
 
     return encrypted;
 }
@@ -30,17 +23,13 @@ DLL2_API std::string Decrypt(std::string str, int key) {
 
     for (int i = 0; i < decrypted.size(); i++)
     {
-        if (std::isalpha(decrypted[i])) {
-            char base = 'a';
-            if (std::isupper(decrypted[i]) == true) {
-                base = 'A';
-            }
-            decrypted[i] = static_cast<char>(base + (decrypted[i] - base - (key % 26) + 26) % 26);
-        }
+        char base = 32; 
+        char end = 126;  
+        decrypted[i] = static_cast<char>(base + (decrypted[i] - base - (key % (end - base + 1)) + (end - base + 1)) % (end - base + 1));
     }
-
 
     return decrypted;
 }
+
 
 
